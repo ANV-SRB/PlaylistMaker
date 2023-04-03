edd45d5a0ce70e85d25a590cfa091fe069601e42
ANV-SRB <anv.vasin@gmail.com>
2023-04-03 19:54:57 +0200

    Добавил UI Тест onClickListener

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //доступ к элементам на activity_main
+        //public <T extends View> T findViewById(@IdRes int id)
+
+        val amBtnFindId = findViewById<Button>(R.id.amBtnFind)
+        val amBtnMediaId = findViewById<Button>(R.id.amBtnMedia)
+        val amBtnSettingsId = findViewById<Button>(R.id.amBtnSettings)
+
+        val toastAmBtnFindId = Toast.makeText(this@MainActivity, "Нажали на amBtnFind!", Toast.LENGTH_SHORT)
+        val toastAmBtnMediaId = Toast.makeText(this@MainActivity, "Нажали на amBtnMedia!", Toast.LENGTH_LONG)
+
+        amBtnFindId.setOnClickListener {
+            toastAmBtnFindId.show()
+        }
+
+        val amBtnFindIdListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                toastAmBtnFindId.show()
+            }
+        }
+        amBtnFindId.setOnClickListener(amBtnFindIdListener)
+
     }
 }
