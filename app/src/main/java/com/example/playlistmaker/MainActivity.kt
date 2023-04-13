b268771427a500d68b86f6a962321b6ddc860dc8
ANV-SRB <anv.vasin@gmail.com>
2023-04-13 11:10:53 +0200

    13/04/23 1. Добавил экраны "Поиск" и "Медиатека" 2. Добавил переходы на все окна и возврат из настроек

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,26 +11,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //доступ к элементам на activity_main
-        //public <T extends View> T findViewById(@IdRes int id)
-
-        val amBtnFindId = findViewById<Button>(R.id.amBtnFind)
+        //доступ к элементам на activity_main   public <T extends View> T findViewById(@IdRes int id)
+        val amBtnSearchId = findViewById<Button>(R.id.amBtnFind)
         val amBtnMediaId = findViewById<Button>(R.id.amBtnMedia)
         val amBtnSettingsId = findViewById<Button>(R.id.amBtnSettings)
 
-        val toastAmBtnFindId = Toast.makeText(this@MainActivity, "Нажали на amBtnFind!", Toast.LENGTH_SHORT)
-        val toastAmBtnMediaId = Toast.makeText(this@MainActivity, "Нажали на amBtnMedia!", Toast.LENGTH_LONG)
+        //val toastAmBtnFindId = Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT)
+       // val toastAmBtnMediaId = Toast.makeText(this@MainActivity, "Нажали на Медиатека!", Toast.LENGTH_LONG)
+        //val toastAmBtnSettingsId = Toast.makeText(this@MainActivity, "Нажали на Настройки!", Toast.LENGTH_SHORT)
 
-        amBtnFindId.setOnClickListener {
-            toastAmBtnFindId.show()
+
+        amBtnSearchId.setOnClickListener {
+            val activitySearchIntentOpen = Intent(this, SearchActivity::class.java)
+            startActivity(activitySearchIntentOpen)
         }
 
-        val amBtnFindIdListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                toastAmBtnFindId.show()
-            }
-        }
-        amBtnFindId.setOnClickListener(amBtnFindIdListener)
 
+        amBtnSettingsId.setOnClickListener {
+            val activitySettingIntentOpen = Intent(this, SettingsActivity::class.java)
+            startActivity(activitySettingIntentOpen)
+        }
+
+        amBtnMediaId.setOnClickListener {
+            val activityMediaIntentOpen = Intent(this, MediaActivity::class.java)
+            startActivity(activityMediaIntentOpen)
+        }
     }
 }
